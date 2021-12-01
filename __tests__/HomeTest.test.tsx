7c0fb309501d3df83312d332526f4b3c7d6a1227
Alvin Naufal <alvin.naufal3@gmail.com>
2021-12-01 22:32:05 +0700

    Fixed navbar test import

--- __tests__/HomeTest.test.tsx
+++ __tests__/HomeTest.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
-import { Navbar } from "../components"
+import Navbar from "../components/navbar";
 
 test("Check navbar menu", () => {
 	const { getByText } = render(<Navbar />);
